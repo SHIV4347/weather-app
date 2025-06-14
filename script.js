65b1418b08c7eadcb5646ad422000fdfb81516e5
SHIV4347 <shivprasad129806@gmail.com>
2025-06-14 14:19:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "f38251fe66025a9067cb166849503da3";
+    const api_key = <! put api key here>
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
